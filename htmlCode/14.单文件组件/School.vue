0b32da8a175611e62515777b41b6c8e4d9324adb
TxpGit <1101642762@qq.com>
2024-05-04 12:33:58 +0800

    单文件组件

---
+++ htmlCode/14.单文件组件/School.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>学生姓名：{{studentName}}</h2>
+        <h2>学生年龄{{age}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data(){
+        return {
+            studentName:'赵六',
+            age:20
+        }
+    },
+    methods: {
+        showName(){
+            alert(this.studentName)
+        }
+    },
+}
+</script>
---
+++ htmlCode/14.单文件组件/Student.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{schoolName}}</h2>
+        <h2>学校地址：{{addr}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    //组件中data必须为函数式
+    data(){
+        return {
+            schoolName:'学校名称',
+            addr:'北京昌平'
+        }
+    },
+    methods: {
+        showName(){
+            alert(this.schoolName)
+        }
+    },
+}
+</script>
+
+<style>
+    .demo{
+        background-color: orange;
+    }
+</style>
